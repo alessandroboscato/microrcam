605f8cd7e54bebb3e1e194b79ddf939fdf906769
Alessandro <alessandro.boscato@gmail.com>
2020-10-08 17:46:09 +0200

    aggiunta path lavori

--- partials/header.php
+++ partials/header.php
@@ -3,7 +3,7 @@
     <nav>
       <ul>
         <li><a href="#">Su di me</a></li>
-        <li><a href="">Alcuni lavori</a></li>
+        <li><a href="http://localhost:8888/microrcam/partials/lavori.php">Alcuni lavori</a></li>
         <li><a href="#">Hard skills</a></li>
         <li><a href="#">Soft skills</a></li>
       </ul>
--- partials/lavori.php
+++ partials/lavori.php
@@ -1,10 +1,10 @@
-<?php var_dump(__DIR__) ?>
-<?php include __DIR__ "head.php" ?>
-<?php include __DIR__ "header.php" ?>
-<?php include __DIR__ "main.php" ?>
+
+<?php include 'head.php' ?>
+<?php include "header.php" ?>
+<?php include "main.php" ?>
 <ul>
   <li>Lorem</li>
   <li>Lorem</li>
   <li>Lorem</li>
 </ul>
-<?php include __DIR__ "footer.php" ?>
+<?php include "footer.php" ?>
